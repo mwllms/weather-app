9678aca40e7e009d95cf97cabd0979e956cabf79
Mark Willems <mrkwillems@gmail.com>
2021-11-05 09:26:53 +0100

    Basic setup

---
+++ src/components/Weather.vue
@@ -0,0 +1,13 @@
+<script setup>
+import axios from 'axios'
+
+const key = import.meta.env.WEATHER_API_KEY
+const city = 'Hilversum'
+
+// const weather = await axios.get(`/forecast.json?key=${key}&q=${city}&days=5&lang=nl`)
+const weather = await axios.get('http://localhost:3000/api/forecast.json')
+</script>
+
+<template>
+  {{ weather }}
+</template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+
+import './assets/tailwind.css'
+
+axios.defaults.baseURL = import.meta.env.WEATHER_API_URL
 
 createApp(App).mount('#app')
